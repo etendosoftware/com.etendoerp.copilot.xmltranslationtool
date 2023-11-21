b93b0728ae10783bc7fc8d82bd1c180bed3e4743
Rocio Altamirano <rocio.altamirano@smfconsulting.es>
2023-11-21 17:40:44 -0300

    Feature EML-396: Lang chain with xml_translator_tool

--- tools/XMLTranslationTool.py
+++ tools/XMLTranslationTool.py
@@ -1,24 +1,24 @@
 import os
+import re
+import xml.dom.minidom
 import xml.etree.ElementTree as ET
+import copy
 import pycountry
 import openai
-from transformers import GPT2Tokenizer
 from copilot.core.tool_wrapper import ToolWrapper
+import time
 from dotenv import load_dotenv
 
 load_dotenv()
 
 class XMLTranslationTool(ToolWrapper):
     name = "XMLTranslationTool"
-    description = "This is a tool that translates the content of XML files from one language to another."
+    description = "This is a tool that receives a relative path and directly translates the content of XML from one language to another, specified within the xml"
+    inputs = ["relative_path"]
+    outputs = ["translated_files_paths"]
 
-
-    def run(self, relative_path: str, *args, **kwargs) -> str:
-        self.tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
-        self.language = "Spanish"
-        self.business_requirement = os.getenv("BUSINESS_TOPIC", "ERP")
+    def run(self, relative_path, *args, **kwargs):
         translated_files_paths = []
-
         script_directory = os.path.dirname(os.path.abspath(__file__))
         first_level_up = os.path.dirname(script_directory)
         second_level_up = os.path.dirname(first_level_up)
@@ -27,21 +27,17 @@ class XMLTranslationTool(ToolWrapper):
         reference_data_path = absolute_path
 
         if not os.path.exists(reference_data_path):
-            return "The mentioned path was not found"
+            raise ValueError(f"The 'referencedata' directory was not found at {reference_data_path}.")
 
         for dirpath, dirnames, filenames in os.walk(reference_data_path):
             xml_files = [f for f in filenames if f.endswith(".xml")]
             for xml_file in xml_files:
                 filepath = os.path.join(dirpath, xml_file)
-                if self.is_already_translated(filepath):
-                    continue
                 translated_file_path = self.translate_xml_file(filepath)
                 if translated_file_path:
                     translated_files_paths.append(translated_file_path)
 
-        if not translated_files_paths:
-            return "The files are already translated"
-        return translated_files_paths
+        return {"translated_files_paths": translated_files_paths}
 
     def get_language_name(self, iso_code):
         language_part = iso_code.split('_')[0]
@@ -50,7 +46,29 @@ class XMLTranslationTool(ToolWrapper):
             return language.name
         return None
 
+    def split_xml_into_segments(self, content, max_tokens):
+            root = ET.fromstring(content)
+            segments = []
+            current_segment = ET.Element(root.tag)
+
+            for child in root:
+                if ET.tostring(current_segment).strip().decode() == f"<{root.tag}></{root.tag}>":
+                    continue
+
+                if len(ET.tostring(current_segment).strip().decode()) + len(ET.tostring(child)) + 2 <= max_tokens:
+                    current_segment.append(child)
+                else:
+                    segments.append(ET.tostring(current_segment).strip().decode())
+                    current_segment = ET.Element(root.tag)
+                    current_segment.append(child)
+
+            if current_segment:
+                segments.append(ET.tostring(current_segment).strip().decode())
+
+            return segments
+
     def translate_xml_file(self, filepath):
+        business_requirement = os.getenv("BUSINESS_TOPIC", "ERP")
         model = os.getenv("OPENAI_MODEL", "gpt-3.5-turbo")
         with open(filepath, "r") as file:
             first_line = file.readline().strip()
@@ -80,13 +98,13 @@ class XMLTranslationTool(ToolWrapper):
 
             original_texts = [value.text for value in value_elements if value.text and value.text.strip()]
             prompt_texts = "\n".join(original_texts)
-            self.prompt = f"""
+            prompt = f"""
             ---
-            Translate the following texts from English to {target_language} in the context of {self.business_requirement} software:
+            Translate the following texts from English to {target_language} in the context of {business_requirement} software:
             {prompt_texts}
             """
 
-            messages = [{"role": "system", "content": self.prompt}]
+            messages = [{"role": "system", "content": prompt}]
             response = openai.ChatCompletion.create(
                 model=model,
                 messages=messages,
@@ -108,17 +126,3 @@ class XMLTranslationTool(ToolWrapper):
                 file.write(translated_text)
 
             return f"Successfully translated file {filepath}."
-
-    def is_already_translated(self, filepath):
-            try:
-                with open(filepath, "r") as file:
-                    root = ET.parse(file).getroot()
-                for child in root:
-                    values = child.findall('value')
-                    for value in values:
-                        if value.get('isTrl', 'N') == 'Y':
-                            return True
-            except ET.ParseError:
-                return False
-            return False
-
